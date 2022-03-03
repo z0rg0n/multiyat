fc086f48e867e01384b98e3c20577b627108e5a3
z0rg0n <82673289+z0rg0n@users.noreply.github.com>
2022-03-02 21:11:16 -0700

    Add files via upload
    Fixed problem with post vs patch in both set and delete methods

--- emoji_id.py
+++ emoji_id.py
@@ -10,77 +10,42 @@ import json
 import requests
 import pprint
 
+
 def store(base_url, account):
-    headers = credentials.create_xapi_head(account) # Make header that will be passed later
-#    print("Enter yat to edit:")
- #   yat = input(": ")#Try 🤘🐺🤘
- #   print("Enter data to be stored:")
- #   address = input(": ") #try 48bPRVkgvHwjG2VUTkPLaGazynZ6RxETuNGsYZNBrtb7ZkAUqY1NE2iGqoLd8EFsvhbDGW8gNb96Jce8fg2aiY8A5mbd8zf
-  #  print("Enter tag (https://api-docs.y.at/docs/categories) of data:")
- #   tag = input(": ")#xmr wallet = 0x1001
-    dict_data ={'insert':[{'data':'48bPRVkgvHwjG2VUTkPLaGazynZ6RxETuNGsYZNBrtb7ZkAUqY1NE2iGqoLd8EFsvhbDGW8gNb96Jce8fg2aiY8A5mbd8zf','tag':'0x1001'}]}
-#Tried above both with [ ] and without
-    print("URL is:")
-    print(base_url+'/emoji_id/🤘🐺🤘')
-    print("sent json_string:")
-    print(dict_data)
-    print(type(dict_data))
-    print("Sent head:")
-    print(headers)
-    count = 0
-    while (count < 2):
-        responce1 = requests.post(base_url + '/emoji_id/🤘🐺🤘', json=dict_data, headers=headers)
-        print("responce json is: ")
-        print(responce1)
-        print(responce1.text)
-        responce = requests.post(base_url+'/emoji_id/🤘🐺🤘', data=dict_data, headers=headers)
-        print("responce data is: ")
-        print(responce)
-        print(responce.text)
-        dict_data = json.dumps(dict_data, skipkeys=True, separators=(',', ':'))
-        dict_data = dict_data.replace("\\", "")
-        count = count + 1
-    print("json string in second two requests:")
-    print(dict_data)
-    print(type(dict_data))
+    headers = credentials.create_xapi_head(account)  # Make header that will be passed later
+    print("Enter yat to edit:")
+    yat = input(": ")  # Try 🤘🐺🤘
+    print("Enter data to be stored:")
+    address = input(
+        ": ")  # try 48bPRVkgvHwjG2VUTkPLaGazynZ6RxETuNGsYZNBrtb7ZkAUqY1NE2iGqoLd8EFsvhbDGW8gNb96Jce8fg2aiY8A5mbd8zf
+    print("Enter tag (https://api-docs.y.at/docs/categories) of data:")
+    tag = input(": ")  # xmr wallet = 0x1001
+    dict_data = {'insert': [{'data': address, 'tag': tag}]}
 
-    params = [{'data':'48bPRVkgvHwjG2VUTkPLaGazynZ6RxETuNGsYZNBrtb7ZkAUqY1NE2iGqoLd8EFsvhbDGW8gNb96Jce8fg2aiY8A5mbd8zf','tag':'0x1001'}]
-    # Tried both with [] on outside making it a list and without
-    payload = {'insert': json.dumps(params, skipkeys=True, separators=(',', ':'))}
-    count = 0
-    while (count < 2):
-        responce1 = requests.post(base_url + '/emoji_id/🤘🐺🤘', json=payload, headers=headers)
-        print("responce json is: ")
-        print(responce1)
-        print(responce1.text)
-        responce = requests.post(base_url+'/emoji_id/🤘🐺🤘', data=payload, headers=headers)
-        print("responce data is: ")
-        print(responce)
-        print(responce.text)
-        payload = json.dumps(payload, skipkeys=True, separators=(',', ':'))
-        payload = payload.replace("\\", "")
-        count = count + 1
-    print("json string in second two requests:")
-    print(payload)
-    print(type(payload))
+    responce = requests.patch(base_url + '/emoji_id/' + yat, json=dict_data, headers=headers)
+    print(responce)
+    print(responce.text)
 
-    return()
+    return ()
+
 
 def emoji_characters(base_url, account):
-    headers = credentials.create_zapi_head(account) # Make header that will be passed later
+    headers = credentials.create_zapi_head(account)  # Make header that will be passed later
     responce = requests.get(base_url + '/emoji', headers=headers)
     obj = json.loads(responce.text)
     print("Avaliable emojis are:")
     print(responce.text)
 
+
 def owned_list(base_url, account):
-    headers = credentials.create_zapi_head(account) # Make header that will be passed later
+    headers = credentials.create_zapi_head(account)  # Make header that will be passed later
     responce = requests.get(base_url + '/emoji_id', headers=headers)
     print(responce)
     obj = json.loads(responce.text)
     print("Owned yats:")
     pprint.pprint(obj)
-    return()
+    return ()
+
 
 def lookup(base_url):
     print("enter a yat to lookup:")
@@ -90,63 +55,25 @@ def lookup(base_url):
     obj = json.loads(responce.text)
     print("Yat info:")
     pprint.pprint(obj)
-    return()
+    return ()
+
 
 def delete_hash(base_url, account):
-    headers = credentials.create_xapi_head(account) # Make header that will be passed later
-#    print("Enter a yat target has is in:")
-#    yat = input(": ")#Try 🤘🐺🤘
-
-#    string_del = ('"delete":["cc99003dc1d247ee990267f5e7c0049971c698b1daefc31fbffa3c2f674c8a32"]')
-#    print("Enter hash to delete:")
-#    target = input(": ")
+    headers = credentials.create_xapi_head(account)  # Make header that will be passed later
+    print("Enter a yat target has is in:")
+    yat = input(": ")  # Try 🤘🐺🤘
+    print("Enter hash to delete:")
+    target = input(": ")  # try cc99003dc1d247ee990267f5e7c0049971c698b1daefc31fbffa3c2f674c8a32
     dict_data = {
         'delete': [
             {
-                'hash': 'cc99003dc1d247ee990267f5e7c0049971c698b1daefc31fbffa3c2f674c8a32'
+                'hash': target
             }
         ]
     }
 
-    print("dict_data:")
-    print(dict_data)
-    print(type(dict_data))
+    responce = requests.post(base_url + '/emoji_id/' + yat, json=dict_data, headers=headers)
+    print(responce)
+    print(responce.text)
 
-    count = 0
-    while (count < 2):
-        responce1 = requests.post(base_url + '/emoji_id/🤘🐺🤘', json=dict_data, headers=headers)
-        print("responce json:")
-        print(responce1)
-        print(responce1.text)
-        responce = requests.post(base_url+'/emoji_id/🤘🐺🤘', data=dict_data, headers=headers)
-        print("responce data:")
-        print(responce)
-        print(responce.text)
-        dict_data = json.dumps(dict_data, skipkeys=True, separators=(',', ':'))
-        dict_data = dict_data.replace("\\", "")
-        count = count + 1
-    print("json string in second two requests:")
-    print(dict_data)
-    print(type(dict_data))
-
-    params = [{'hash':'cc99003dc1d247ee990267f5e7c0049971c698b1daefc31fbffa3c2f674c8a32'}]
-    # tried above both with [ ] and without on outside making it a list.
-    payload = {'delete': json.dumps(params, skipkeys=True, separators=(',', ':'))}
-    count = 0
-    while (count < 2):
-        responce1 = requests.post(base_url + '/emoji_id/🤘🐺🤘', json=payload, headers=headers)
-        print("responce json:")
-        print(responce1)
-        print(responce1.text)
-        responce = requests.post(base_url+'/emoji_id/🤘🐺🤘', data=payload, headers=headers)
-        print("responce data:")
-        print(responce)
-        print(responce.text)
-        payload = json.dumps(payload, skipkeys=True, separators=(',', ':'))
-        payload = payload.replace("\\", "")
-        count = count + 1
-    print("json payload in second two requests:")
-    print(payload)
-    print(type(payload))
-
-    return()
+    return ()
